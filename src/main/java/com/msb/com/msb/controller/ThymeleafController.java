faadde4fb67bdb8613946c98e61cbaba68cf98ca
xurb <xurongbiao@hmtim.com>
2022-05-27 23:53:42 +0800

    前端设置插件

--- src/main/java/com/msb/com/msb/controller/ThymeleafController.java
+++ src/main/java/com/msb/com/msb/controller/ThymeleafController.java
@@ -2,12 +2,17 @@ package com.msb.com.msb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class ThymeleafController {
 
+//    @ResponseBody
     @RequestMapping("/show")
-    public String show(){
+    public String show(Map<String ,Object> map){
+        map.put("msg","testMessage");
         return "index";
     }
 
